6a8f705daae61728fca2a065c9d243fa85f5b721
MaganaAsiati <assia.magana@gmail.com>
2022-06-01 12:13:38 +0200

    importe route in the index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import Calculator from './components/Calculator';
+import './App.css';
+import Home from './components/home';
+import Quote from './components/Quote';
+
+document.body.style = 'background-color: #131313;';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="home" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="Quotes" element={<Quote />} />
+      </Route>
+    </Routes>
+  </Router>,
 );
